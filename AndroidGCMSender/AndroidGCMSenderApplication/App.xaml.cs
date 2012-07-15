5849154fde9b0a0523a5de054776c87a8040aa56
SND\modalove_cp <SND\modalove_cp@aadf2aa7-86a3-4242-a263-039aa7a476c3>
2012-07-15 15:15:28 +0000

    Init project
    git-svn-id: https://gcm.svn.codeplex.com/svn@18986 aadf2aa7-86a3-4242-a263-039aa7a476c3

---
+++ AndroidGCMSender/AndroidGCMSenderApplication/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace AndroidGCMSenderApplication
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static class GCMResult
+        {
+            public static string Json { get; internal set; }
+        }
+    }
+}
---
+++ AndroidGCMSender/GCMSender/GCMSender.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace GCMSender
+{
+    public class GCMSender
+    {
+        private const string GCM_URI = "https://android.googleapis.com/gcm/send";
+
+        #region Properties
+        public string DeviceToken { get; set; }
+        public string APIKey { get; set; } 
+        #endregion
+
+        public GCMSender()
+        {
+            // do nothing
+        }
+
+        public GCMSender(string deviceToken, string apiKey)
+        {
+            DeviceToken = deviceToken;
+            APIKey = apiKey;
+        }
+
+        public string Send(string message)
+        {
+
+
+            return null;
+        }
+
+        private string AssembleJSONPayload(string gcmDeviceToken, string gcmBody)
+        {
+            string payloadFormat =
+                "{{" +
+                    "\"registration_ids\" : [\"{0}\"]," +
+                    "\"data\" : {{" +
+                        " {1} " +
+                    "}}" +
+                "}}";
+
+            string payload = string.Format(payloadFormat, gcmDeviceToken, gcmBody);
+            Debug.WriteLine("payload : " + payload);
+
+            return payload;
+        }
+    }
+}
